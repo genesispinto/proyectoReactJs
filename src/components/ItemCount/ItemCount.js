8da15bb85352a75a36b9e5a23e3f93ef15a7d147
genesis pinto <genesisgps@gmail.com>
2022-03-31 12:14:34 -0300

    se agrega componente ItemCount

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./ItemCount.css";
+
+const ItemCount = (props) =>{
+
+    const [count, setCount]= useState(props.initial)
+    const [stock, setStock]= useState(props.stock1)
+
+    const Decrement = ()=>{
+        if(count >0) {
+            setCount(count - 1)
+            setStock(stock + 1)
+        }
+
+    }
+    const Increment = ()=>{
+        if (stock > 0){
+            setCount(count + 1)
+            setStock(stock - 1)
+        }
+        
+    }
+    return(
+        <div className="controlesProductos">
+            <button onClick={Decrement}>-</button>
+            <p>{count}</p>
+            <button onClick={Increment}>+</button>
+            <button className="agregaCarrito">Agregar a Carrito</button>
+            <p className="stock">STOCK: {stock}</p>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,15 @@
+import "./ItemListContainer.css";
+import ItemCount from "../ItemCount/ItemCount";
+
 const ItemListContainer = (props)=>{
     return(
-        <p>{props.title}</p>
+        <div className="productos">
+            <h1>{props.title}</h1>
+            <ItemCount stock1= {10} initial = {0}/>
+                
+        </div>
+        
+        
     )
 }
 export default ItemListContainer
